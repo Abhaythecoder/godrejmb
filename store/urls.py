2a7c9820516f6e757543b4416f31b2784f7ffc68
Abhaythecoder <88576691+Abhaythecoder@users.noreply.github.com>
2025-09-16 19:59:11 +0530

    removing functions for data retrieval

--- store/urls.py
+++ store/urls.py
@@ -3,18 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# --- START: Code for temporary DB download ---
-import uuid
-# Make sure this import points to the views.py file where you added the function
-from app.views import secret_db_download_view
-
-# Generate a unique, random path that no one can guess
-secret_path = f'download-db-backup-file/{uuid.uuid4()}/'
-
-# This line will print the secret path in your Render deploy logs so you can find it
-print(f"SECRET DATABASE DOWNLOAD URL PATH IS: {secret_path}")
-# --- END: Code for temporary DB download ---
-
 
 urlpatterns = [
     # Add the secret path to your url patterns
